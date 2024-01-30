9cb4d7ad1e7801b004ac4b1073d8c47d92917df2
Moises Diaz <Moises.diaz9108@gmail.com>
2024-01-30 11:38:42 -0500

    labs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(list, result){
+    return list.filter((possibleMatch) => possibleMatch.toLowerCase() === result.toLowerCase());
+}
+
+function fuzzyMatch(list, target){
+    return list.filter((placeholder)=> placeholder.toLowerCase().indexOf(target.toLowerCase())=== 0);
+}
+
+function matchName(list, targetName){
+    return list.filter((placeholder)=>placeholder.name === targetName);
+}
